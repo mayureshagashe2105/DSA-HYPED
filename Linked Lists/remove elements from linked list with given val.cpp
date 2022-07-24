fedc332f63596e1c493e996f1b977c6bd038ead2
Mayuresh Agashe <75118658+mayureshagashe2105@users.noreply.github.com>
2022-07-24 10:41:55 +0530

    Create remove elements from linked list with given val.cpp

---
+++ Linked Lists/remove elements from linked list with given val.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode* temp = head, *prev = head;
+        while(temp != NULL)
+        {
+            if (temp->val == val)
+            {
+                if (temp->next == NULL)
+                {
+                    if(temp == head)head = head->next;
+                    prev->next = NULL;
+                    break;
+                }
+                else
+                {
+                    temp->val = temp->next->val;
+                    temp->next = temp->next->next;
+                }
+            }
+            else
+            {
+                prev = temp;
+                temp = temp->next;
+            }
+        }
+        return head;
+    }
+};
