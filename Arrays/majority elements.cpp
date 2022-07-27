3f25a1c560042d40b39865b6f25bf8049791e788
Mayuresh Agashe <75118658+mayureshagashe2105@users.noreply.github.com>
2022-07-27 09:54:16 +0530

    Create majority elements.cpp

---
+++ Arrays/majority elements.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count = 0, ele;
+        
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            if(count == 0) ele = nums[i];
+            if(nums[i] == ele) ++count;
+            else --count;
+        }
+        return ele;
+    }
+};
