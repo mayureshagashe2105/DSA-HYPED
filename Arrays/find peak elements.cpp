8ebe6931ecb9cc37797f18c0d4309a827e391466
Mayuresh Agashe <75118658+mayureshagashe2105@users.noreply.github.com>
2022-07-27 09:44:54 +0530

    Update find peak elements.cpp

--- Arrays/find peak elements.cpp
+++ Arrays/find peak elements.cpp
@@ -17,3 +17,27 @@ public:
         return 0;
     }
 };
+
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int start = 0, end = nums.size() - 1;
+        
+        if (nums.size() == 1) return 0;
+        if (nums[0] > nums[1]) return 0;
+        if (nums[end] > nums[end - 1]) return end;
+        
+        
+        while(end >= start)
+        {
+            int mid = (end + start) / 2;
+            if (nums[mid] > nums[mid + 1] && nums[mid] > nums[mid - 1]) return mid;
+            else if (nums[mid] < nums[mid + 1]) start = mid + 1;
+            else if (nums[mid] < nums[mid - 1]) end = mid - 1;
+            // cout << mid << end << start << endl;
+
+        }
+        return -1;
+    }
+};
